1a74f34a977fc0f57970950e8be3ab5a043c61a6
TriciaSutor <p.sutor@wustl.edu>
2026-03-25 10:37:34 -0500

    Added hovers

--- view.js
+++ view.js
@@ -72,3 +72,21 @@ $('.permbutton').click( function( e ) {
 
 // ---- Assign unique ids to everything that doesn't have an ID ----
 $('#html-loc').find('*').uniqueId() 
+
+const allowHover = document.getElementById('perm_entry_header_allow');
+const allowExplanation = document.getElementById('perm_entry_header_allow_explanation');   
+const denyHover = document.getElementById('perm_entry_header_deny');
+const denyExplanation = document.getElementById('perm_entry_header_deny_explanation');
+
+allowHover.addEventListener('mouseenter', function() {
+    allowExplanation.style.display = 'block';
+});
+allowHover.addEventListener('mouseleave', function() {
+    allowExplanation.style.display = 'none';
+});
+denyHover.addEventListener('mouseenter', function() {
+    denyExplanation.style.display = 'block';
+});
+denyHover.addEventListener('mouseleave', function() {
+    denyExplanation.style.display = 'none';
+});
