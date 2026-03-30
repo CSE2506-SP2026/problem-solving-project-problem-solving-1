b75a3cd5cd08485334c579627cb2fe2b09569f22
TriciaSutor <p.sutor@wustl.edu>
2026-03-29 22:26:07 -0500

    Added descriptive buttons

--- view_baseline.js
+++ view_baseline.js
@@ -17,7 +17,7 @@ perm_dialog = define_new_dialog('permdialog', title='Permissions', options = {
     },
     buttons: {
         OK:{
-            text: "OK",
+            text: "Save Changes",
             id: "perm-dialog-ok-button",
             click: function() {
                 $( this ).dialog( "close" );
